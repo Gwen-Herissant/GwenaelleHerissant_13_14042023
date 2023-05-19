093316e4db27e4eed915921bf89de2ff245b645b
Gwenaëlle Herissant <gh@pointligneplan.fr>
2023-05-19 15:10:51 +0200

    Added  "Remember Me" functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,17 @@
 import { useDispatch } from "react-redux";
 import "../styles/main.css";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { login} from "../features/loginData";
 
 export default function Form(){
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const storedUsername = localStorage.getItem('username');
+  const [rememberMe, setRememberMe] = useState(Boolean(storedUsername));
+
   const dispatch = useDispatch()
 
   const navigate = useNavigate();
@@ -19,7 +23,18 @@ export default function Form(){
       .then((result) => {
         navigate('/profile');
       });
+    
+    if (rememberMe) {
+      localStorage.setItem('username', username);
+    } else {
+      localStorage.removeItem('username');
+    }
   };
+
+  useEffect(() => {
+    setUsername(storedUsername || '');
+    setRememberMe(Boolean(storedUsername));
+  }, [storedUsername]);
 
   return(
     <form>
@@ -32,7 +47,7 @@ export default function Form(){
         <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
       <div className="input-remember">
-        <input type="checkbox" id="remember-me" /><label htmlFor="remember-me">Remember me</label>
+        <input type="checkbox" id="remember-me" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} /><label htmlFor="remember-me">Remember me</label>
       </div>
       <button className="sign-in-button" type="button" onClick={handleClick}>Sign In</button>
     </form>
